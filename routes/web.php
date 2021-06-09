65b1a6aa2bf50a8159aea31710a77a8545c9eeb2
kekeli543 <kekeli543@gmail.com>
2021-06-09 17:01:15 +0800

    修改路由表

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts','App\Http\Controllers\PostController@index');
+Route::post('/posts/store','App\Http\Controllers\PostController@store');
+Route::match(['get','post'],'/posts/{id}','App\Http\Controllers\PostController@show');
